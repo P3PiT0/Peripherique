a1309fd50df054adad94d3a2e359b6763e8230e8
LUCAS_CHZ <chambaz@etud.insa-toulouse.fr>
2021-09-19 14:31:50 +0200

    première partie GPIO

--- Projet2/Include/MYGPIO.h
+++ Projet2/Include/MYGPIO.h
@@ -2,19 +2,19 @@
 #define MYGPIO_H
 #include "stm32f10x.h"
 
-#define In_Floating 0b01 
-#define In_PullDown 0b10 
-#define In_PullUp 0b10
-#define In_Analog 0b00
-#define Out_Ppull 0b00 
-#define Out_OD 0b01
-#define AltOut_Ppull 0b10
-#define AltOut_OD 0b11
+#define In_Floating 0x1 
+#define In_PullDown 0x2 
+#define In_PullUp 0x2
+#define In_Analog 0x0
+#define Out_Ppull 0x0 
+#define Out_OD 0x1
+#define AltOut_Ppull 0x2
+#define AltOut_OD 0x3
 
-#define ModInput 0b00
-#define Mod10 0b01
-#define Mod2 0b10
-#define Mod50 0b11
+#define ModInput 0x0
+#define Mod10 0x1
+#define Mod2 0x2
+#define Mod50 0x3
  
 
 void MyGPIO_Init (GPIO_TypeDef* GPIO, int Pin, int Mode, int CRLH ) ; // Configuration du PIN
--- Projet2/Source/Principal.c
+++ Projet2/Source/Principal.c
@@ -1,11 +1,9 @@
 #include "stm32f10x.h"
 #include "MYGPIO.h" 
 
-
-
 int main (void)
 {
-	MyGPIO_Init(GPIOC,5,1,1) ; //Mod10,Out_Ppull);
+	MyGPIO_Init(GPIOC,5,Mod10,Out_Ppull);
 	MyGPIO_Set(GPIOC, 5);
 	MyGPIO_Reset(GPIOC, 5);
 	MyGPIO_Toggle(GPIOC, 5);
